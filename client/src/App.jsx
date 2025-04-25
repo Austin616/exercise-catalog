f634b7c2bba9e7f7379b5f26b316a4f3c6261570
Austin Tran <austintran@wireless-10-155-41-15.public.utexas.edu>
2025-04-25 14:41:55 -0500

    Set up tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='bg-lime-700'>
+      Tailwind CSS is working!
+      
+    </div>
+  )
+}
+
+export default App
